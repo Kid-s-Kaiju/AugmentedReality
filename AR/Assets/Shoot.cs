46c00a9e9985d2fc57426a3dd02998d7bba483c3
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2020-05-20 17:21:58 +0200

    project

---
+++ AR/Assets/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject shot = Instantiate(bullet, transform.position, Quaternion.identity);
+                Rigidbody rb = shot.GetComponent<Rigidbody>();
+                shot.transform.LookAt(hit.point);
+                rb.velocity = rb.transform.forward * 10;
+            }
+        }
+    }
+}
