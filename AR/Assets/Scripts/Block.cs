38673b1f2e1cdd36a6c4cc082f7d3e4251099231
sherzock <jacobo.galofre@gmail.com>
2020-05-21 21:50:55 +0200

    Animation before disappearing

--- AR/Assets/Scripts/Block.cs
+++ AR/Assets/Scripts/Block.cs
@@ -7,31 +7,45 @@ public class Block : MonoBehaviour
     private Vector3 startPosition;
     private float lastCheck;
     private bool hasFallen = false;
+    private Animator animatorr;
+    bool ReadyToDestroy = false;
 
     private void Start()
     {
         startPosition = transform.position;
+        animatorr = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (hasFallen)
             return;
 
-        if (gameObject.GetComponent<MeshRenderer>().enabled)
-        {
-            gameObject.GetComponent<Rigidbody>().useGravity = true;
-            if (Time.time - lastCheck > 1)
+            if (gameObject.GetComponent<MeshRenderer>().enabled)
             {
-                lastCheck = Time.time;
-
-                if (Vector3.Magnitude(transform.position - startPosition) > 1)
+                gameObject.GetComponent<Rigidbody>().useGravity = true;
+                if (Time.time - lastCheck > 1)
                 {
-                    hasFallen = true;
-                    GameManager.Instance.RemoveBlock(gameObject);
+                    lastCheck = Time.time;
+
+                    if (Vector3.Magnitude(transform.position - startPosition) > 1)
+                    {
+                        hasFallen = true;
+                        animatorr.SetBool("FadeStart", true);
+                        StartCoroutine(DestroyBlock());
+                    }
                 }
             }
-        }
     }
+
+    private IEnumerator DestroyBlock()
+    {
+        yield return new WaitForSeconds(5);
+        GameManager.Instance.RemoveBlock(gameObject);
+        Destroy(gameObject);
+    }
+
+
 }
+
+
